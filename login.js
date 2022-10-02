2bd62c3f25fe280bdf1d0ee98d9aefd9a72542d4
dAtLIcKK421503912fo3e35fjdgket353kjJ3l <114768890+dAtLIcKK421503912fo3e35fjdgket353kjJ3l@users.noreply.github.com>
2022-10-02 08:54:45 -0500

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 let encoded-password = ("YTI5NWU0MiFAc2xvcEVHYW1lISQ=");
-let decoded-password = window.atob(encoded-password);
+let decoded-password = window.btoa(encoded-password);
 //a295e42!@slopEGame!$
 loginButton.addEventListener("click", (e) => {
    e.preventDefault();
