bdd044e9c24a71957420c30daac961dee08257d3
dAtLIcKK421503912fo3e35fjdgket353kjJ3l <114768890+dAtLIcKK421503912fo3e35fjdgket353kjJ3l@users.noreply.github.com>
2022-10-03 08:47:41 -0500

    Update login.js

--- login.js
+++ login.js
@@ -13,7 +13,7 @@ loginButton.addEventListener("click", (e) => {
       document.cookie = "login=True";
       let loginTrue = getCookie("login");
       alert(loginTrue);
-      if (loginTrue === "login=True") {
+      if (loginTrue === "True") {
          window.location.replace("https://datlickk421503912fo3e35fjdgket353kjj3l.github.io/Ko3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjf/unity.html");
          //figure out how to delete the cookie here so you can't access again (security)
       } else {
