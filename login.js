19fe386f9899d7112f80924052d9febcea88a283
dAtLIcKK421503912fo3e35fjdgket353kjJ3l <114768890+dAtLIcKK421503912fo3e35fjdgket353kjJ3l@users.noreply.github.com>
2022-10-03 07:06:08 -0500

    Update login.js

--- login.js
+++ login.js
@@ -3,14 +3,20 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 let encodedPassword = ("YTI5NWU0MiFAc2xvcEVHYW1lISQ=");
 let decodedPassword = window.btoa(encodedPassword);
-//a295e42!@slopEGame!$
+
 loginButton.addEventListener("click", (e) => {
    e.preventDefault();
    const username = loginForm.username.value;
    const password = loginForm.password.value;
    if (username === "ls120cbaJp" && password === "a295e42!@slopEGame!$") {
       alert("You have successfully logged in.");
-      window.location.refresh("unity.html");
+      document.cookie(login=True)
+      let loginTrue = getCookie("login");
+      if (loginTrue === "True") {
+         window.location.replace("https://datlickk421503912fo3e35fjdgket353kjj3l.github.io/Ko3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjfKo3h2hlrkjdhflkj3hklgrjf/unity.html")
+         //figure out how to delete the cookie here so you can't access again (security)
+      } else {
+         alert("No brute forcing :/")
    } else {
       loginErrorMsg.style.opacity = 1;
    }
